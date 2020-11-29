63da37a8902928175d2408d386993e529a9e7f1e
Will Andre <wandre94@gmail.com>
2020-11-29 19:17:22 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -2,7 +2,8 @@ board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
 def turn_count(board)
   counter = 0
-  if board.each == !" "
-  counter +=1
+  if board.each == !" " do
+    counter += 1
+  end
 end
 end
