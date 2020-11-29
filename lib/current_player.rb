daac81bb13c925597c5eee50f7f2fdb93c900cd5
Will Andre <wandre94@gmail.com>
2020-11-29 19:34:54 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -3,7 +3,8 @@ def turn_count(board)
   counter = 0
   board.each do |spaces|
     if spaces == "X" || spaces == "O"
-      counter += 1
+      return counter += 1
+      until counter = 9
     end
 end
 end
